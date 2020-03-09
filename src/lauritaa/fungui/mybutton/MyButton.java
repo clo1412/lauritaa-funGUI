5c89421ea1d11a65f70e48899d27caff33a416fe
clo1412 <chyntia1412.cl@gmail.com>
2020-03-09 12:47:04 +0700

    create folder mybutton and file MyButton.java

---
+++ src/lauritaa/fungui/mybutton/MyButton.java
@@ -0,0 +1,13 @@
+package mybutton;
+
+import javax.swing.JButton;
+
+public class MyButton extends JButton {
+	
+	private static final long serialVersionUID = 1L;
+
+	public MyButton() {
+		this.setText("Hack");
+		this.setBounds(70, 10, 100, 25);
+	}
+}
