32836a7fba454a65358510430110de869e97abdf
clo1412 <chyntia1412.cl@gmail.com>
2020-03-09 12:49:12 +0700

    create folder mylabel and file MyLabel.java

---
+++ src/lauritaa/fungui/mylabel/MyLabel.java
@@ -0,0 +1,13 @@
+package mylabel;
+
+import javax.swing.JLabel;
+
+public class MyLabel extends JLabel{
+	
+	private static final long serialVersionUID = 1L;
+
+	public MyLabel() {
+		this.setText("Start Hacking");
+		this.setBounds(20, 45, 210, 35);
+	}
+}
